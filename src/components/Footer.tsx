d6de17f5321edad7f2b072d7769f54907d041985
syg0629 <syg0629@hanmail.net>
2024-03-27 17:52:39 +0900

    Refactor: 변수명 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,11 @@ import "./Footer.css";
 
 const Footer = () => {
   const { pathname } = useLocation();
-  const showAddBtnPath = ["/notice", "/wod", "/record", "/hold", "/shop"];
+  const pathsToShowWriteBtn = ["/notice", "/wod", "/record", "/hold", "/shop"];
 
   return (
     <footer className="footer">
-      {showAddBtnPath.includes(pathname) && (
+      {pathsToShowWriteBtn.includes(pathname) && (
         <div className="footer_write_button">
           <WriteButton />
         </div>
