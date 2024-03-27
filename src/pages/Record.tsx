131e757e4b4688415125faf4dffffe15ee456c95
syg0629 <syg0629@hanmail.net>
2024-03-27 11:36:50 +0900

    Feat: Record 페이지 및 스타일 추가

--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -1,5 +1,25 @@
+import "./Record.css";
+import Table from "../components/Table";
+
+const tableTopTitle = ["Rx’d", "A"];
+const tableBottomTitle = ["B", "C"];
+
 const Record = () => {
-  return <div>Record</div>;
+  return (
+    <div className="record_wrapper">
+      <h1 className="title">Record</h1>
+      <div className="record_grade_top">
+        {tableTopTitle.map((title, idx) => (
+          <Table key={idx} title={title} />
+        ))}
+      </div>
+      <div className="record_grade_botton">
+        {tableBottomTitle.map((title, idx) => (
+          <Table key={idx} title={title} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Record;
