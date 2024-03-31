b99984ebc80812fb365097b5c0791d0e78da61c2
syg0629 <syg0629@hanmail.net>
2024-03-31 22:21:50 +0900

    Feat: supabase 설정으로 모든 기록 조회

--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -1,23 +1,45 @@
 import "./Record.css";
+import WriteButton from "../components/WriteButton";
 import Table from "../components/Table";
+import { Database } from "../api/supabase/supabase";
+import { supabase } from "../api/supabase/supabaseClient";
+import { useEffect, useState } from "react";
 
-const tableTopTitle = ["Rx’d", "A"];
-const tableBottomTitle = ["B", "C"];
+type Record = Database["public"]["Tables"]["record"]["Row"];
 
 const Record = () => {
+  const [records, setRecords] = useState<Record[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: records, error } = await supabase
+          .from("record")
+          .select("*")
+          .order("record", { ascending: true });
+
+        if (error) {
+          throw error;
+        }
+        setRecords(records);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log("Supabase 데이터 가져오는 중 오류 >> ", error.message);
+        }
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="record_wrapper">
       <h1 className="title">Record</h1>
       <div className="record_grade_top">
-        {tableTopTitle.map((title, idx) => (
-          <Table key={idx} title={title} />
+        {records.map((record: Record) => (
+          <Table key={record.id} {...record} />
         ))}
       </div>
-      <div className="record_grade_botton">
-        {tableBottomTitle.map((title, idx) => (
-          <Table key={idx} title={title} />
-        ))}
-      </div>
+      <WriteButton writePath="/record/writerecord" />
     </div>
   );
 };
