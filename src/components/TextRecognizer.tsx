c86df257d85a97dda95a05d8249473fae9624626
syg0629 <syg0629@hanmail.net>
2024-04-12 17:30:59 +0900

    Refactor: useQuery 적용

--- src/components/TextRecognizer.tsx
+++ src/components/TextRecognizer.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
 import "./TextRecognizer.css";
 import Tesseract from "tesseract.js";
+import { useQuery } from "@tanstack/react-query";
 
 interface TextRecognizerProps {
   imgUrl: string;
@@ -15,38 +15,33 @@ const TextRecognizer = ({
   type,
   onTextRecognition,
 }: TextRecognizerProps) => {
-  const [recognizedText, setRecognizedText] = useState<string>("");
+  const { data: recognizedText, isLoading } = useQuery({
+    queryKey: ["recognizeImgText", imgUrl],
+    queryFn: async () => {
+      if (imgUrl) {
+        return await recognizeImgText(imgUrl, id, type);
+      }
+    },
+  });
 
-  useEffect(() => {
-    let isMounted = true;
-    if (imgUrl) {
-      recognizeImgText(imgUrl, id, type);
+  const recognizeImgText = async (imgUrl: string, id: number, type: string) => {
+    try {
+      const result = await Tesseract.recognize(imgUrl, "eng+kor");
+
+      if (result && result.data.text) {
+        onTextRecognition(result.data.text, id, type);
+        return result.data.text;
+      }
+    } catch (error) {
+      console.log("텍스트 추출 중 에러 >> ", (error as Error).message);
+      throw error;
     }
-    return () => {
-      isMounted = false;
-    };
-  }, [imgUrl, id, type]);
-
-  const recognizeImgText = (imgUrl: string, id: number, type: string) => {
-    Tesseract.recognize(imgUrl, "eng+kor", {
-      logger: ({ status, progress }) => {
-        if (status === "recognizing text") {
-          // 텍스트 인식 진행률
-          const progressPercentage = Number(progress.toFixed(2)) * 100;
-          console.log(`진행률: ${progressPercentage}%`);
-        }
-      },
-    })
-      .catch((error: Error) => {
-        console.log("텍스트 추출 중 에러 >> ", error.message);
-      })
-      .then((result) => {
-        if (result && result.data.text) {
-          setRecognizedText(result.data.text);
-          onTextRecognition(result.data.text, id, type);
-        }
-      });
   };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return <div className="write_record_tesseract">{recognizedText}</div>;
 };
 export default TextRecognizer;
