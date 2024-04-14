35726e791822a304b876e1b21aa72c1db68c453b
syg0629 <syg0629@hanmail.net>
2024-04-14 23:03:05 +0900

    Refactor: 배열 반환, 오류 처리방식 수정

--- src/utils/handleSupabaseResponse.ts
+++ src/utils/handleSupabaseResponse.ts
@@ -1,14 +1,11 @@
 import { PostgrestError } from "@supabase/supabase-js";
 
 export const handleSupabaseResponse = <T>(
-  data: T | null,
+  data: T[] | null,
   error: PostgrestError | null
-): T => {
+): T[] => {
   if (error) {
     throw error;
   }
-  if (!data) {
-    throw new Error("Data is null");
-  }
-  return data;
+  return data ?? [];
 };
