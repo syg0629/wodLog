fa317e40f9f2f2e8c47bdf1e521de3025cad0153
syg0629 <syg0629@hanmail.net>
2024-04-16 22:00:12 +0900

    Refactor: 기록을 workoutType에 따라 그룹화하여 테이블 컴포넌트에 전달

--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -29,13 +29,31 @@ const Record = () => {
     },
   });
 
+  const groupedRecords = records?.reduce((acc, record) => {
+    if (!acc[record.workoutType]) {
+      acc[record.workoutType] = [];
+    }
+    acc[record.workoutType].push(record);
+    return acc;
+  }, {} as { [key: string]: Record[] });
+
   return (
     <div className="record_wrapper">
       <h1 className="title">Record</h1>
-      <div className="record_grade_top">
-        {records?.map((record: Record) => (
-          <Table key={record.id} {...record} />
-        ))}
+      <div className="record_grade">
+        {records && records.length > 0 ? (
+          Object.entries(groupedRecords).map(
+            ([workoutType, workoutRecords]) => (
+              <Table
+                key={workoutType}
+                workoutType={workoutType}
+                records={workoutRecords}
+              />
+            )
+          )
+        ) : (
+          <div className="no_record">등록된 기록이 없습니다!</div>
+        )}
       </div>
       <ActionButton>
         <Link to="/record/write" className="write_button">
