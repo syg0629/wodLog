7a9cd483c6f22dcb3cb69a4a46d50016dbf71832
syg0629 <syg0629@hanmail.net>
2024-05-29 13:57:37 +0900

    Refactor: deltaToHtml 적용으로 인한 코드 정리

--- src/pages/Notice/Notice.tsx
+++ src/pages/Notice/Notice.tsx
@@ -2,36 +2,29 @@ import "./Notice.css";
 import "../../components/common/Common.css";
 import { Link, useLocation } from "react-router-dom";
 import ActionButton from "../../components/common/ActionButton";
-import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 import NoticeItem from "../../pages/Notice/NoticeItem";
 import Line from "../../components/common/Line";
 import { noticeQueryKeys } from "../../queries/noticeQueries";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { Database } from "../../api/supabase/supabase";
+import { FaPencil } from "react-icons/fa6";
+
+type Notice = Database["public"]["Tables"]["notice"]["Row"];
 
 const Notice = () => {
   // 경로에 따라 Notice wrapper CSS를 다르게 설정하기 위한 코드
   const { pathname } = useLocation();
   const noticeWrapperClassName =
-    pathname === "/" ? "home_notice_wrapper" : "notice_wrapper";
+    pathname === "/" ? "home_notice_wrapper" : "wrapper";
 
   const { data: notice } = useSuspenseQuery(noticeQueryKeys.list());
-
-  // delta로 저장된 contents를 html로 변환
-  const deltaToHtmlData = notice.map((post) => {
-    const postContent = post.content;
-    const deltaOps = JSON.parse(postContent).ops;
-    const deltaToHtmlConverter = new QuillDeltaToHtmlConverter(deltaOps, {});
-    const html = deltaToHtmlConverter.convert();
-    return { ...post, content: html };
-  });
 
   return (
     <div className={noticeWrapperClassName}>
       <h1 className="title">Notice</h1>
-      {/* <h4 className="notice_count">총 {notice.length}개의 글이 있습니다🏋🏻‍♀️</h4> */}
 
       <div>
-        {deltaToHtmlData.map((post) => (
+        {notice.map((post) => (
           <div key={post.id}>
             <NoticeItem {...post} />
             <Line />
@@ -41,7 +34,7 @@ const Notice = () => {
 
       <ActionButton>
         <Link to="/notice/write" className="write_button">
-          +
+          <FaPencil />
         </Link>
       </ActionButton>
     </div>
