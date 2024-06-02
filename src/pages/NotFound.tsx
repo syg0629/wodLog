5d4ffdb3cdc35e3472d2164b0c5e1e8d7d18c3f7
syg0629 <syg0629@hanmail.net>
2024-06-02 22:51:18 +0900

    Style: 잘못된 접근 페이지 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,21 @@
+import { TiWarningOutline } from "react-icons/ti";
+import "./NotFound.css";
+
 const NotFound = () => {
-  return <div>잘못된 페이지입니다.</div>;
+  const onClickGoBack = () => {
+    window.history.back();
+  };
+
+  return (
+    <div className="not_found">
+      <TiWarningOutline />
+      <div>잘못된 접근입니다.</div>
+      <div>찾으시는 페이지가 존재하지 않습니다.</div>
+      <button className="go_back_btn" onClick={onClickGoBack}>
+        이전 페이지로
+      </button>
+    </div>
+  );
 };
 
 export default NotFound;
