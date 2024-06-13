750c83c174e8f2e0d43e022eb5f29993dd468186
syg0629 <syg0629@hanmail.net>
2024-06-13 22:09:23 +0900

    Refactor: useSuspenseQuery로 수정

--- src/pages/Hold/EditHold.tsx
+++ src/pages/Hold/EditHold.tsx
@@ -1,18 +1,13 @@
 import WriteHold from "../../pages/Hold/WriteHold";
 import { useParams } from "react-router-dom";
 import { holdQueryKeys } from "../../queries/holdQueries";
-import { useQuery } from "@tanstack/react-query";
-import Loader from "../../components/common/Loader";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 const EditHold = () => {
   const params = useParams();
   const holdId = Number(params.id);
 
-  const { data: editHoldData } = useQuery(holdQueryKeys.detail(holdId));
-
-  if (!editHoldData) {
-    return <Loader />;
-  }
+  const { data: editHoldData } = useSuspenseQuery(holdQueryKeys.detail(holdId));
 
   return <WriteHold isEdit={true} data={editHoldData} />;
 };
