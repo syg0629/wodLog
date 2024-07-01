1e3fdc33c8e6f51618c2ea1be93257aaa251ea3b
syg0629 <syg0629@hanmail.net>
2024-07-01 17:58:01 +0900

    Refactor: 목록, 상세, 저장 공통 함수 생성으로 Notice Query 수정

--- src/queries/noticeQueries.ts
+++ src/queries/noticeQueries.ts
@@ -1,36 +1,18 @@
-import { PostgrestSingleResponse } from "@supabase/supabase-js";
-import { supabase } from "../api/supabase/supabaseClient";
 import { createQueryKeys } from "@lukemorales/query-key-factory";
-import { handleSupabaseResponse } from "../utils/handleSupabaseResponse";
-import { deltaToHtml } from "../utils/deltaToHtml";
 import { Notice } from "../types/type";
+import { createDetailQueryFn, createListQueryFn } from "./createQueryFns";
 
 export const noticeQueryKeys = createQueryKeys("notice", {
-  // Notice/Notice
+  // ContentList
   // Notice 목록
   list: () => ({
     queryKey: ["all"],
-    queryFn: async (): Promise<Notice[]> => {
-      const data = await supabase
-        .from("notice")
-        .select("*")
-        .order("id", { ascending: false });
-      const notice = await handleSupabaseResponse(data);
-      return deltaToHtml(notice);
-    },
+    queryFn: createListQueryFn<Notice>("notice"),
   }),
-  // Notice/DetailNotice
-  // Notice/EditNotice
-  // Notice 상세 페이지 / Notice 수정시 기존 값 조회
+  // DetailContent, EditContent
+  // Notice 상세 페이지 / Notice 수정 시 기존 값 조회
   detail: (noticeId: number) => ({
     queryKey: [noticeId],
-    queryFn: async () => {
-      const data: PostgrestSingleResponse<Notice[]> = await supabase
-        .from("notice")
-        .select("*")
-        .eq("id", noticeId);
-      const detailNotice = await handleSupabaseResponse(data);
-      return deltaToHtml(detailNotice);
-    },
+    queryFn: () => createDetailQueryFn<Notice>("notice")(noticeId),
   }),
 });
