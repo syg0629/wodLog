9ebacb8e43e3b79c7e24cc2aca19c3c5018869db
syg0629 <syg0629@hanmail.net>
2024-07-02 15:40:17 +0900

    Refactor: CSS 속성을 사용하여 길이 제한 함수 삭제

--- src/components/common/Content/ContentItem.tsx
+++ src/components/common/Content/ContentItem.tsx
@@ -18,21 +18,13 @@ const ContentItem = ({
     navigate(`/${contentType}/${id}`);
   };
 
-  // title, content Preview로 text 길이 제한
-  // html코드를 제거하고 공백을 넣어 텍스트만 남기고 그 길이가 길 경우 잘라내고 ...로 보여주는 코드
-  const stripHtmlAndPreview = (text: string, maxLength: number) => {
-    const strippedText = text.replace(/<[^>]+>/g, " ");
-    return strippedText.length > maxLength
-      ? strippedText.slice(0, maxLength) + "..."
-      : strippedText;
-  };
-  const titlePreview = stripHtmlAndPreview(title, 22);
-  const contentPreview = stripHtmlAndPreview(content, 70);
+  // Notice Content Preview: html코드를 제거하고 공백을 넣어 텍스트만 추출
+  const strippedContent = content.replace(/<[^>]+>/g, " ");
 
   return (
     <article className="article" onClick={onClickMoveToDetail}>
       <div className="article_head">
-        <h1 className="article_title">{titlePreview}</h1>
+        <h1 className="article_title">{title}</h1>
         <div className="article_write_info">
           <span className="article_writer">{writer}</span>
           <span className="article_date">
@@ -44,7 +36,7 @@ const ContentItem = ({
         <div
           className="article_content_preview"
           dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(contentPreview),
+            __html: DOMPurify.sanitize(strippedContent),
           }}
         />
       )}
