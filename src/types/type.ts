2922c86d4925dbef4bd0cf9e178bb4934e7941b7
syg0629 <syg0629@hanmail.net>
2024-07-16 12:56:32 +0900

    Feat: Record 타입 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -3,6 +3,7 @@ import { Database } from "../api/supabase/supabase";
 export type Notice = Database["public"]["Tables"]["notice"]["Row"];
 export type Wod = Database["public"]["Tables"]["wod"]["Row"];
 export type Hold = Database["public"]["Tables"]["hold"]["Row"];
+export type Record = Database["public"]["Tables"]["record"]["Row"];
 
 export type Holiday = {
   locdate: number;
