b6ca21523fcfb60a17acd4d32ba74186bb4f0898
syg0629 <syg0629@hanmail.net>
2024-07-20 22:08:17 +0900

    Feat: 로그인 상태에 따른 메뉴 표시 및 로그아웃 기능 추가, 웹 접근성과 시멘틱 태그에 맞게 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,31 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
 import weightlift from "../../assets/weightLifting.svg";
+import { isLoggedAtom, logoutAtom } from "../../store/atoms";
+import { useAtom } from "jotai";
 
 const Header = () => {
+  const [isLogged] = useAtom(isLoggedAtom);
+  const [, logout] = useAtom(logoutAtom);
+
+  const handleLogout = async () => {
+    if (isLogged) {
+      await logout();
+    }
+  };
+
   return (
     <header className="header">
       <Link to="/" className="header_logo">
         wodLog
-        <img className="weightLiftIcon" src={weightlift} />
+        <img
+          className="weightLiftIcon"
+          src={weightlift}
+          alt="Weightlifting Icon"
+        />
         &nbsp;&nbsp;
       </Link>
-      <div className="header_menu_wrapper">
+      <nav className="header_menu_wrapper">
         <Link to="/notice" className="header_menu_btn">
           NOTICE
         </Link>
@@ -20,13 +35,25 @@ const Header = () => {
         <Link to="/record" className="header_menu_btn">
           RECORD
         </Link>
-        <Link to="/hold" className="header_menu_btn">
-          HOLD
-        </Link>
-        <Link to="/login" className="header_menu_btn_login">
-          LOGIN
-        </Link>
-      </div>
+        {isLogged && (
+          <Link to="/hold" className="header_menu_btn">
+            HOLD
+          </Link>
+        )}
+        {isLogged ? (
+          <Link
+            to="/login"
+            className="header_menu_btn_login"
+            onClick={handleLogout}
+          >
+            LOGOUT
+          </Link>
+        ) : (
+          <Link to="/login" className="header_menu_btn_login">
+            LOGIN
+          </Link>
+        )}
+      </nav>
     </header>
   );
 };
