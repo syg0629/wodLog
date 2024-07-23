8313965ffd609272509927675eaddc4a0800c126
syg0629 <syg0629@hanmail.net>
2024-07-24 00:37:36 +0900

    Refactor: 로그아웃 상태면 null로 명확하게 처리, 변수명 수정, 로그인 상태 확인 accessTokenAtom 반환으로 수정

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -10,14 +10,10 @@ export const accessTokenAtom = atomWithStorage<string | null>(
 );
 
 //로그인 시 받아오는 유저 정보
-export const userInfoAtom = atom<UserInfo>({
-  userName: "",
-  writerUuid: "",
-  auth: "",
-});
+export const userInfoAtom = atom<UserInfo | null>(null);
 
 //세션 확인 및 유저 정보 설정
-export const userAuthAtom = atom(null, async (_, set) => {
+export const baseUserAuthAtom = atom(null, async (_, set) => {
   try {
     const { data, error } = await supabase.auth.getSession();
     if (error) throw error;
@@ -42,20 +38,20 @@ export const userAuthAtom = atom(null, async (_, set) => {
   } catch (error) {
     console.error("로그인 세션 에러 >> ", error);
     set(accessTokenAtom, null);
-    set(userInfoAtom, { userName: "", writerUuid: "", auth: "" });
+    set(userInfoAtom, null);
   }
 });
 
 //로그인 상태 확인 및 로그아웃
-export const loginLogoutAtom = atom(
-  //로그인 상태 확인
-  (get) => !!get(accessTokenAtom),
+export const userAuthAtom = atom(
+  //로그인 상태 확인(accessToken 반환)
+  (get) => get(accessTokenAtom),
   //로그아웃
   async (_, set) => {
     try {
       await supabase.auth.signOut();
       set(accessTokenAtom, null);
-      set(userInfoAtom, { userName: "", writerUuid: "", auth: "" });
+      set(userInfoAtom, null);
     } catch (error) {
       console.error("로그아웃 중 에러 발생 >> ", error);
     }
