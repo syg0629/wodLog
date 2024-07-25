c5f964330168297e53a3e91baa98fc6b727cff2c
syg0629 <syg0629@hanmail.net>
2024-07-25 23:28:28 +0900

    Refactor: Record페이지 - 쿼리 키 관리를 위해 코드 분리, 기록을 workoutType 순으로 정렬하여 표시, 사용자 권한 반영

--- src/pages/Record/RecordList.tsx
+++ src/pages/Record/RecordList.tsx
@@ -1,35 +1,19 @@
-import "./RecordList.css";
+import "./Record.css";
 import "../../components/common/Common.css";
 import ActionButton from "../../components/common/ActionButton";
 import RecordTable from "../../components/common/RecordTable";
-import { Database } from "../../api/supabase/supabase";
-import { supabase } from "../../api/supabase/supabaseClient";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
-import { handleSupabaseResponse } from "../../utils/handleSupabaseResponse";
-import { PostgrestSingleResponse } from "@supabase/supabase-js";
 import { FaPencil } from "react-icons/fa6";
 import NoPost from "../../components/common/NoPost";
-
-type Record = Database["public"]["Tables"]["record"]["Row"];
+import { Record } from "../../types/type";
+import { userInfoAtom } from "../../store/atoms";
+import { useAtom } from "jotai";
+import { recordQueryKeys } from "../../queries/recordQueries";
 
 const RecordList = () => {
-  const { data: records } = useSuspenseQuery<
-    Record[],
-    Error,
-    Record[],
-    string[]
-  >({
-    queryKey: ["records"],
-    queryFn: async () => {
-      const data: PostgrestSingleResponse<Record[]> = await supabase
-        .from("record")
-        .select("*")
-        .order("record", { ascending: true });
-
-      return handleSupabaseResponse(data);
-    },
-  });
+  const [userInfo] = useAtom(userInfoAtom);
+  const { data: records } = useSuspenseQuery(recordQueryKeys.list());
 
   const groupedRecords = records?.reduce((acc, record) => {
     if (!acc[record.workoutType]) {
@@ -39,6 +23,10 @@ const RecordList = () => {
     return acc;
   }, {} as { [key: string]: Record[] });
 
+  const sortedRecords = ["Rx’d", "A", "B", "C"]
+    .filter((type) => groupedRecords && groupedRecords[type])
+    .map((type) => [type, groupedRecords[type]] as [string, Record[]]);
+
   return (
     <div className="wrapper">
       <h1 className="title">Record</h1>
@@ -46,7 +34,7 @@ const RecordList = () => {
         {records && records.length > 0 ? (
           <>
             <div className="record_grade_1">
-              {Object.entries(groupedRecords)
+              {sortedRecords
                 .slice(0, 2)
                 .map(([workoutType, workoutRecords]) => (
                   <RecordTable
@@ -57,7 +45,7 @@ const RecordList = () => {
                 ))}
             </div>
             <div className="record_grade_2">
-              {Object.entries(groupedRecords)
+              {sortedRecords
                 .slice(2, 4)
                 .map(([workoutType, workoutRecords]) => (
                   <RecordTable
@@ -72,11 +60,14 @@ const RecordList = () => {
           <NoPost post="Record" />
         )}
       </div>
-      <ActionButton>
-        <Link to="/record/write" className="write_button">
-          <FaPencil />
-        </Link>
-      </ActionButton>
+
+      {userInfo?.auth === "admin" && (
+        <ActionButton>
+          <Link to="/record/write" className="write_button">
+            <FaPencil />
+          </Link>
+        </ActionButton>
+      )}
     </div>
   );
 };
