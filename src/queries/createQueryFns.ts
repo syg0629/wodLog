bd2da6c2e5732f5de03e7c205623d921b0da0128
syg0629 <syg0629@hanmail.net>
2024-07-25 20:44:56 +0900

    Refactor: 사용자 정보 조회 쿼리에 추가, 게시물 저장 시 사용자 정보도 함께 저장

--- src/queries/createQueryFns.ts
+++ src/queries/createQueryFns.ts
@@ -2,7 +2,7 @@ import { supabase } from "../api/supabase/supabaseClient";
 import { handleSupabaseResponse } from "../utils/handleSupabaseResponse";
 import { deltaToHtml } from "../utils/deltaToHtml";
 import { PostgrestSingleResponse } from "@supabase/supabase-js";
-import { Content } from "../types/type";
+import { Content, ContentWithUserInfo } from "../types/type";
 
 type Tables = "notice" | "wod";
 
@@ -10,7 +10,7 @@ export const createListQueryFn =
   (table: Tables) => async (): Promise<Content[]> => {
     const data = await supabase
       .from(table)
-      .select("*")
+      .select("*, userInfo(userName)")
       .order("id", { ascending: false });
     const response = await handleSupabaseResponse(data);
     return deltaToHtml(response);
@@ -19,15 +19,24 @@ export const createListQueryFn =
 export const createDetailQueryFn =
   (table: Tables) =>
   async (id: number): Promise<Content> => {
-    const data = await supabase.from(table).select("*").eq("id", id);
+    const data = await supabase
+      .from(table)
+      .select("*, userInfo(userName, auth)")
+      .eq("id", id);
     const response = await handleSupabaseResponse(data);
     return deltaToHtml(response)[0];
   };
 
 export const createSaveQueryFn =
-  <T extends Content>(table: Tables, isEdit: boolean, contentId: number) =>
+  <T extends ContentWithUserInfo>(
+    table: Tables,
+    isEdit: boolean,
+    contentId: number
+  ) =>
   async (data: T): Promise<T> => {
-    const { title, content, writer, createdDate } = data;
+    const { title, content, createdDate } = data;
+    const writerUuid = data.userInfo.writerUuid;
+
     const savedData: PostgrestSingleResponse<T[]> = isEdit
       ? await supabase
           .from(table)
@@ -36,7 +45,7 @@ export const createSaveQueryFn =
           .select()
       : await supabase
           .from(table)
-          .insert([{ title, content, writer, createdDate }])
+          .insert([{ title, content, writerUuid, createdDate }])
           .select();
 
     return (await handleSupabaseResponse(savedData))[0];
