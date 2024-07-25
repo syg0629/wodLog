dfa58c1c7504672a138c16e51aa8a43357d5ef51
syg0629 <syg0629@hanmail.net>
2024-07-25 23:35:09 +0900

    Refactor: Record페이지 - 쿼리 키 관리 코드 분리, useSuspenseQuery로 변경, 텍스트 인식 후 한 번만 콜백 호출되도록 상태 추가

--- src/pages/Record/TextRecognizer.tsx
+++ src/pages/Record/TextRecognizer.tsx
@@ -1,7 +1,7 @@
-import "./TextRecognizer.css";
-import Tesseract from "tesseract.js";
-import { useQuery } from "@tanstack/react-query";
-import Loader from "../../components/common/Loader";
+import "./Record.css";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { recordQueryKeys } from "../../queries/recordQueries";
+import { useEffect, useState } from "react";
 
 interface TextRecognizerProps {
   imgUrl: string;
@@ -16,32 +16,17 @@ const TextRecognizer = ({
   type,
   onTextRecognition,
 }: TextRecognizerProps) => {
-  const { data: recognizedText, isLoading } = useQuery({
-    queryKey: ["recognizeImgText", imgUrl],
-    queryFn: async () => {
-      if (imgUrl) {
-        return await recognizeImgText(imgUrl, id, type);
-      }
-    },
+  const [isRegistered, setIsRegistered] = useState(false);
+  const { data: recognizedText } = useSuspenseQuery({
+    ...recordQueryKeys.recognizeText(imgUrl, id, type),
   });
 
-  const recognizeImgText = async (imgUrl: string, id: number, type: string) => {
-    try {
-      const result = await Tesseract.recognize(imgUrl, "eng+kor");
-
-      if (result && result.data.text) {
-        onTextRecognition(result.data.text, id, type);
-        return result.data.text;
-      }
-    } catch (error) {
-      console.log("텍스트 추출 중 에러 >> ", (error as Error).message);
-      throw error;
+  useEffect(() => {
+    if (recognizedText && !isRegistered) {
+      onTextRecognition(recognizedText, id, type);
+      setIsRegistered(true);
     }
-  };
-
-  if (isLoading) {
-    return <Loader />;
-  }
+  }, [recognizedText, id, type, onTextRecognition, isRegistered]);
 
   return <div className="write_record_tesseract">{recognizedText}</div>;
 };
