6348b936730718329618c34831e40bce3bc3ae01
syg0629 <syg0629@hanmail.net>
2024-07-25 22:51:00 +0900

    Refactor: Hold페이지 - 사용자 정보 추가, 잔여 홀드일로 요청일을 바로 게산할 수 있도록 수정

--- src/pages/Hold/HoldList.tsx
+++ src/pages/Hold/HoldList.tsx
@@ -6,15 +6,26 @@ import { holdQueryKeys } from "../../queries/holdQueries";
 import Table from "../../components/common/Table";
 import dayjs from "dayjs";
 import { useMemo } from "react";
-import { Hold } from "../../types/type";
+import { HoldWithUserInfo } from "../../types/type";
+import { useAuthenticatedUserInfo } from "../../hooks/useAuthenticatedUserInfo";
 
 const HoldList = () => {
+  const userInfo = useAuthenticatedUserInfo();
   const navigate = useNavigate();
-  const onClickMoveToDetail = (id: number) => {
-    navigate(`/hold/${id}/edit`);
-  };
 
-  const { data: hold } = useSuspenseQuery(holdQueryKeys.list());
+  const { data: hold } = useSuspenseQuery(holdQueryKeys.list(userInfo));
+
+  const handleMoveToDetail = (id: number) => {
+    const canEdit = hold.some(
+      (holdData) =>
+        holdData.id === id && userInfo.writerUuid === holdData.writerUuid
+    );
+    if (canEdit) {
+      navigate(`/hold/${id}/edit`);
+    } else {
+      alert("본인이 작성한 Hold만 수정 가능합니다.");
+    }
+  };
 
   //Hold 신청일의 끝 날짜 기준으로 정렬
   const sortedData = useMemo(() => {
@@ -28,19 +39,19 @@ const HoldList = () => {
 
   // Hold table body
   const renderBody = (
-    sortedData: Hold[],
-    onClickMoveToDetail: (id: number) => void
+    sortedData: HoldWithUserInfo[],
+    handleMoveToDetail: (id: number) => void
   ) => (
     <tbody>
       {sortedData.map((holdData, index) => (
-        <tr key={holdData.id} onClick={() => onClickMoveToDetail(holdData.id)}>
+        <tr key={holdData.id} onClick={() => handleMoveToDetail(holdData.id)}>
           <td>{index + 1}</td>
-          <td>{holdData.writer}</td>
+          <td>{holdData.userInfo.userName}</td>
           <td>
             {holdData.holdStartDay} ~ {holdData.holdEndDay}
           </td>
           <td>{holdData.requestedHoldDays} 일</td>
-          <td>{holdData.remainingHoldDays} 일</td>
+          <td>{holdData.userInfo.remainingHoldDays} 일</td>
         </tr>
       ))}
     </tbody>
@@ -57,7 +68,7 @@ const HoldList = () => {
             data={sortedData}
             renderBody={renderBody}
             post="Hold"
-            onClickMoveToDetail={onClickMoveToDetail}
+            handleMoveToDetail={handleMoveToDetail}
           />
         </div>
       </div>
