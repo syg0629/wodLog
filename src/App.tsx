cac361aaaafe3fbfce5d5027c28325955e6f722f
syg0629 <syg0629@hanmail.net>
2024-07-29 19:13:08 +0900

    Refactor: 불필요한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Suspense } from "react";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
@@ -26,7 +25,6 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <Header />
       <Suspense fallback={<Loader />}>
         <Routes>
