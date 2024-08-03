cd8dc5ec57f9051935e8fe0dbc18f6d3d0a1be7f
syg0629 <syg0629@hanmail.net>
2024-08-03 23:39:34 +0900

    Refactor: 로그인 상태 확인 atom 작성,

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -10,45 +10,50 @@ export const accessTokenAtom = atomWithStorage<string | null>(
 );
 
 // 사용자 정보 저장 atom
-export const userInfoAtom = atom<UserInfo | null>(null);
+export const userInfoAtom = atomWithStorage<UserInfo | null>("userInfo", null);
 
-// 로그인 상태 확인 및 사용자 정보 설정
-export const userAuthAtom = atom(
-  // accessToken 반환
-  (get) => get(accessTokenAtom),
-  // 세션 확인 및 사용자 정보 설정
-  async (_, set) => {
-    try {
-      const { data, error } = await supabase.auth.getSession();
-      if (error) throw error;
+// 로그인 상태 확인 atom
+export const isAuthenticatedAtom = atom((get) => {
+  const accessToken = get(accessTokenAtom);
+  const userInfo = get(userInfoAtom);
+  return !!accessToken && !!userInfo;
+});
 
-      if (data.session?.access_token) {
-        set(accessTokenAtom, data.session.access_token);
+// 사용자 인증 세션 확인 및 사용자 정보 초기화 atom
+export const userAuthAtom = atom(null, async (get, set) => {
+  // 이미 인증된 상태
+  if (get(isAuthenticatedAtom)) return;
 
-        const { data: userInfoData, error: userInfoError } = await supabase
-          .from("userInfo")
-          .select("userName, auth")
-          .eq("id", data.session?.user.id)
-          .single();
+  try {
+    const { data, error } = await supabase.auth.getSession();
+    if (error) throw error;
 
-        if (userInfoError) throw userInfoError;
+    if (data.session?.access_token) {
+      set(accessTokenAtom, data.session.access_token);
 
-        set(userInfoAtom, {
-          userName: userInfoData.userName,
-          writerUuid: data.session.user.id,
-          auth: userInfoData.auth,
-        });
-      } else {
-        set(accessTokenAtom, null);
-        set(userInfoAtom, null);
-      }
-    } catch (error) {
-      console.error("로그인 세션 에러 >> ", error);
+      const { data: userInfoData, error: userInfoError } = await supabase
+        .from("userInfo")
+        .select("userName, auth")
+        .eq("id", data.session?.user.id)
+        .single();
+
+      if (userInfoError) throw userInfoError;
+
+      set(userInfoAtom, {
+        userName: userInfoData.userName,
+        writerUuid: data.session.user.id,
+        auth: userInfoData.auth,
+      });
+    } else {
       set(accessTokenAtom, null);
       set(userInfoAtom, null);
     }
+  } catch (error) {
+    console.error("로그인 세션 에러 >> ", error);
+    set(accessTokenAtom, null);
+    set(userInfoAtom, null);
   }
-);
+});
 
 // 로그아웃
 export const logoutAtom = atom(null, async (_, set) => {
