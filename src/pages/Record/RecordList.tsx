26a4c651319c2152eb0b8f8103533d1b65e2102c
syg0629 <syg0629@hanmail.net>
2024-08-06 22:49:04 +0900

    Refactor: 기록 측정 방식에 따라 순위 정렬 방식 개선

--- src/pages/Record/RecordList.tsx
+++ src/pages/Record/RecordList.tsx
@@ -10,22 +10,64 @@ import { Record } from "../../types/type";
 import { userInfoAtom } from "../../store/atoms";
 import { useAtomValue } from "jotai";
 import { recordQueryKeys } from "../../queries/recordQueries";
+import { useMemo } from "react";
+
+type GroupedRecords = { [key: string]: Record[] };
+
+const workoutTypeOrder = ["Rx’d", "A", "B", "C"];
+const recordTypeOrder = ["time", "round", "number", "unrecognizable"];
 
 const RecordList = () => {
   const userInfo = useAtomValue(userInfoAtom);
   const { data: records } = useSuspenseQuery(recordQueryKeys.list());
 
-  const groupedRecords = records?.reduce((acc, record) => {
-    if (!acc[record.workoutType]) {
-      acc[record.workoutType] = [];
-    }
-    acc[record.workoutType].push(record);
-    return acc;
-  }, {} as { [key: string]: Record[] });
+  const groupedAndSortedRecords = useMemo(() => {
+    // workoutType 별로 records 그룹화
+    const groupedRecords = records.reduce((acc: GroupedRecords, record) => {
+      if (!acc[record.workoutType]) {
+        acc[record.workoutType] = [];
+      }
+      acc[record.workoutType].push(record);
+      return acc;
+    }, {});
 
-  const sortedRecords = ["Rx’d", "A", "B", "C"]
-    .filter((type) => groupedRecords && groupedRecords[type])
-    .map((type) => [type, groupedRecords[type]] as [string, Record[]]);
+    // workoutType 내에서 records를 정렬
+    Object.keys(groupedRecords).forEach((workoutType) => {
+      groupedRecords[workoutType].sort((a, b) => {
+        // recordType 순서 비교
+        const typeComparison =
+          recordTypeOrder.indexOf(a.recordType) -
+          recordTypeOrder.indexOf(b.recordType);
+        if (typeComparison !== 0) return typeComparison;
+
+        // recordType이 같은 경우, sortableRecord 값으로 정렬
+        // time 오름차순, round 및 number 내림차순
+        return a.recordType === "time"
+          ? a.sortableRecord - b.sortableRecord
+          : b.sortableRecord - a.sortableRecord;
+      });
+    });
+    return groupedRecords;
+  }, [records]);
+
+  // 존재하는 workoutType만 화면에 보여줌
+  const sortedWorkoutTypes = workoutTypeOrder
+    .filter((type) => groupedAndSortedRecords[type])
+    .map((type) => [type, groupedAndSortedRecords[type]] as [string, Record[]]);
+
+  const renderRecordTables = (start: number, end: number) => (
+    <div className={`record_grade_${start + 1}`}>
+      {sortedWorkoutTypes
+        .slice(start, end)
+        .map(([workoutType, workoutRecords]) => (
+          <RecordTable
+            key={workoutType}
+            workoutType={workoutType}
+            records={workoutRecords}
+          />
+        ))}
+    </div>
+  );
 
   return (
     <div className="wrapper">
@@ -33,28 +75,8 @@ const RecordList = () => {
       <div className="record_grade">
         {records && records.length > 0 ? (
           <>
-            <div className="record_grade_1">
-              {sortedRecords
-                .slice(0, 2)
-                .map(([workoutType, workoutRecords]) => (
-                  <RecordTable
-                    key={workoutType}
-                    workoutType={workoutType}
-                    records={workoutRecords}
-                  />
-                ))}
-            </div>
-            <div className="record_grade_2">
-              {sortedRecords
-                .slice(2, 4)
-                .map(([workoutType, workoutRecords]) => (
-                  <RecordTable
-                    key={workoutType}
-                    workoutType={workoutType}
-                    records={workoutRecords}
-                  />
-                ))}
-            </div>
+            {renderRecordTables(0, 2)}
+            {renderRecordTables(2, 4)}
           </>
         ) : (
           <NoPost post="Record" />
