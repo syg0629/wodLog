143ca884ced9512c8e3fa2ad228071ad97009a13
syg0629 <syg0629@hanmail.net>
2024-08-12 18:38:24 +0900

    Refactor: 소셜 로그인에서 provider 구분을 위한 state 피라미터 추가

--- src/utils/oAuth.ts
+++ src/utils/oAuth.ts
@@ -2,5 +2,5 @@ export const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_REST_API_KEY;
 export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
 
-export const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login`;
-export const GOOGLE_AUTH_URI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email profile`;
+export const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&prompt=login&state=kakao`;
+export const GOOGLE_AUTH_URI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email profile&state=google`;
