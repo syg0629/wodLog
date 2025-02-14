9f39c8f2a0ab731e8d780cbb042a517a26415bd1
syg0629 <syg0629@hanmail.net>
2025-02-14 23:33:38 +0900

    Fix: eslint rules 수정 - 'React' must be in scope when using JSX

--- eslint.config.js
+++ eslint.config.js
@@ -7,12 +7,13 @@ import pluginReact from "eslint-plugin-react";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
-  {
-    rules: {
-      "react/react-in-jsx-scope": "off",
-    },
-  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
 ];
