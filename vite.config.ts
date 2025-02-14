4bbfc1296ea6469fab2c925d219af6e4c4a83ac9
syg0629 <syg0629@hanmail.net>
2025-02-15 00:20:13 +0900

    Refactor: package 및 vite config 정리

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
 });
